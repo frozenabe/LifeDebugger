6de9a204a7e313cfd56b5271f367d25bbe976472
Abe <frozenabe86@hotmail.com>
2017-10-20 00:17:48 +0900

    Update user.js

--- server/resources/models/user.js
+++ server/resources/models/user.js
@@ -7,12 +7,12 @@ const userSchema = mongoose.Schema({
   password: String,
 });
 
-userSchema.methods.generateHash = function(password) {
+userSchema.methods.generateHash = (password) => {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // checking if password is valid
-userSchema.methods.validPassword = function(password) {
+userSchema.methods.validPassword = (password) => {
   return bcrypt.compareSync(password, this.password);
 };
 
