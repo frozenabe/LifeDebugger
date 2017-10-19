5743ae4554207466bdfcc3238f8bc57bcd55133b
Abe <frozenabe86@hotmail.com>
2017-10-20 00:18:14 +0900

    Update user.js

--- server/resources/models/user.js
+++ server/resources/models/user.js
@@ -11,10 +11,8 @@ userSchema.methods.generateHash = (password) => {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
-// checking if password is valid
 userSchema.methods.validPassword = (password) => {
   return bcrypt.compareSync(password, this.password);
 };
 
-// create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
