01c95c94d7cbd0a671e366275dac130ba0c5ff4b
Abe <frozenabe86@hotmail.com>
2017-10-20 00:26:10 +0900

    Update addGroupBtn.js

--- client/src/components/addGroupBtn.js
+++ client/src/components/addGroupBtn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const AddGroupBtn = ({addGroup}) => (
+const AddGroupBtn = ({ addGroup }) => (
   <a onClick={addGroup} className="addGroupBtn"><img src="../../img/groupAdd.svg" /></a>
 )
 
