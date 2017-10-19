a52ad4eb59ca44e95661e6fb38d22e20c92b9533
Abe <frozenabe86@hotmail.com>
2017-10-20 00:16:46 +0900

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,18 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const mongoUri = 'mongodb://ec2-13-124-163-154.ap-northeast-2.compute.amazonaws.com/lifedebugger';
-
-
+const mongoUri = 'mongodb://127.0.0.1:27017/lifedebugger';
 const db = mongoose.connection;
 
-db.on('error', () => {
-  console.error.bind(console, 'connection error:') 
-});
-
-db.once('open', () => {
-  console.log("Connected to DB");
-});
+db.on('error', () => console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log("Connected to DB"));
 
 mongoose.connect(mongoUri, {useMongoClient: true});
 
